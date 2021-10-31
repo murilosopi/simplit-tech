b0f62884e324ab239d7d0169591da29a4e880bb6
Murilo Sopi <dev.murilosopi@gmail.com>
2021-10-31 18:48:52 -0300

    Adiciona informaçoes em .member-box e finaliza formatacao essencial

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
-document.addEventListener('DOMContentLoaded',function(event){
-    if (document.title=="Home"){
-    var dataText = [ "Conheça: Cloud Computing"];
-    } else if (document.title=="Quem Somos") {
-      var dataText = [ "Quem Somos"];
+document.addEventListener('DOMContentLoaded', function(event) {
+    if (document.title == "Home") {
+        var dataText = ["Conheça: Cloud Computing"];
+    } else if (document.title == "Quem Somos") {
+        var dataText = ["Quem Somos"];
     }
 
     function typeWriter(text, i, fnCallback) {
-      if (i < (text.length)) {
-       document.querySelector(".typedtext").innerHTML = text.substring(0, i+1) +'<span id="cursor" aria-hidden="true"></span>';
-  
-        setTimeout(function() {
-          typeWriter(text, i + 1, fnCallback)
-        }, 100);
-      }
-      else if (typeof fnCallback == 'function') {
-        setTimeout(fnCallback, 700);
-      }
+        if (i < (text.length)) {
+            document.querySelector(".typedtext").innerHTML = text.substring(0, i + 1) + '<span id="cursor" aria-hidden="true"></span>';
+
+            setTimeout(function() {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        } else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 700);
+        }
     }
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 20000);
-       }
-      if (i < dataText[i].length) {
-       typeWriter(dataText[i], 0, function(){
-         StartTextAnimation(i + 1);
-       });
-      }
+
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function() {
+                StartTextAnimation(0);
+            }, 20000);
+        }
+        if (i < dataText[i].length) {
+            typeWriter(dataText[i], 0, function() {
+                StartTextAnimation(i + 1);
+            });
+        }
     }
     StartTextAnimation(0);
-  });
+});
